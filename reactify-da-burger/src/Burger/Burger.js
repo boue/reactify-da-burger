fbcade4a29529d5836db37ad33c33cb9509831b3
Germain Boue <boue.germain@gmail.com>
2018-03-02 20:52:17 -0800

    Add card styling to the Burger component

--- reactify-da-burger/src/Burger/Burger.js
+++ reactify-da-burger/src/Burger/Burger.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Burger.css';
 
 const burger = (props) => {
   return (
-    <div>
+    <div className="Burger">
       <p>This is the {props.name} burger and it contains {props.ingredients} ingredients!</p>
     </div> 
   );
