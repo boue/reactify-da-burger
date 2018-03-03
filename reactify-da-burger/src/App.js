bfac85a57f3bba7a9d86e150f45483b4046a902d
Germain Boue <boue.germain@gmail.com>
2018-03-02 17:58:14 -0800

    Add state to the App which holds burgers available by default

--- reactify-da-burger/src/App.js
+++ reactify-da-burger/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Burger from './Burger/Burger';
 
 class App extends Component {
+  state = {
+    burgers: [{ name: 'Supreme', ingredients: 5 }]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a Burger Making App</h1>
-        <Burger name="Supreme" ingredients="5" />
+        <Burger 
+          ingredients={this.state.burgers[0].ingredients}
+          name={this.state.burgers[0].name}  
+        />
       </div>
     );
   }
