1f6d305305b57bc83b34c70d6fdb0f12443c2ead
Germain Boue <boue.germain@gmail.com>
2018-03-02 18:22:32 -0800

    Create a start over button which is connect to a burger handler method that goes back to default start of creation

--- reactify-da-burger/src/App.js
+++ reactify-da-burger/src/App.js
@@ -7,10 +7,19 @@ class App extends Component {
     burgers: [{ name: 'Supreme', ingredients: 5 }]
   }
 
+  switchBurgerHandler = () => {
+    this.setState({ 
+      burgers: [
+        { name: 'yet to be made ', ingredients: 0}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a Burger Making App</h1>
+        <button onClick={this.switchBurgerHandler} >Start Over</button>
         <Burger 
           ingredients={this.state.burgers[0].ingredients}
           name={this.state.burgers[0].name}  
